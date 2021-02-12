7e3e5953da75d53eaaa07eb4d4b0621e2c5616b1
Serg Molchanov <smolchanov64@gmail.com>
2021-02-12 22:53:35 +0300

    6 task

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -30,5 +30,38 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+    
+    let tHead = document.createElement('thead');
+    let tBody = document.createElement('tbody');
+
+    tHead.innerHTML = `<tr>
+                    <th>Имя</th>
+                    <th>Возраст</th>
+                    <th>Зарплата</th>
+                    <th>Город</th>
+                    <th></th>
+                    </tr>`;
+
+    this.elem.append(tHead);
+
+    for (let row of rows) {
+      let bodyRow = document.createElement('tr');
+      bodyRow.innerHTML = `<td>${row.name}</td>
+                          <td>${row.age}</td>
+                          <td>${row.salary}</td>
+                          <td>${row.city}</td>
+                          <td><button class = 'row-button'>X</button></td>`;
+      tBody.append(bodyRow);
+    }
+    this.elem.append(tBody);    
+
+    this.elem.onclick = this.removeRow;    
+  }
+  removeRow = function(event) {    
+    if (event.target.classList.contains('row-button')) {
+      let row = event.target.closest("tr");
+      row.remove();
+    }
   }
 }

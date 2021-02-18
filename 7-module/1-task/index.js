fdb1ba05f2a7b5c0fb9afd58027da82d2cd78691
Serg Molchanov <smolchanov64@gmail.com>
2021-02-18 14:21:43 +0300

    7 task

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -3,5 +3,81 @@ import createElement from '../../assets/lib/create-element.js';
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+
+    this.elem = createElement('<div class="ribbon"></div>');
+
+    this.leftButton = createElement(
+      `<button class="ribbon__arrow ribbon__arrow_left">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>`);
+
+    this.rightButton = createElement(
+      `<button class="ribbon__arrow ribbon__arrow_right  ribbon__arrow_visible">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>`);
+
+    this.inner = createElement('<nav class="ribbon__inner"></nav>');
+
+    for (let category of categories) {
+      let categoryRef = createElement(
+        `<a href="#" class="ribbon__item" data-id="${category.id}">${category.name}</a>`);
+      if (!category.id) {
+        categoryRef.classList.add('ribbon__item_active');
+        this.activeItem = categoryRef;
+      }
+      this.inner.append(categoryRef);
+    }
+
+    this.elem.append(this.leftButton);
+    this.elem.append(this.inner);
+    this.elem.append(this.rightButton);
+
+    this.elem.addEventListener('click', this.onClick);
+    this.inner.addEventListener('scroll', this.onScrollInner);
+  }
+
+  onClick = (event) => {
+    
+    let button = event.target.closest('.ribbon__arrow');
+    if (button) {
+      if (button.classList.contains('ribbon__arrow_right')) {
+        this.inner.scrollBy(350,0);
+      } else {
+        this.inner.scrollBy(-350,0);
+      }
+      return;
+    }
+
+    let categoryRef = event.target.closest('.ribbon__item');
+    if (categoryRef) {
+
+      event.preventDefault();
+      
+      this.activeItem.classList.remove('ribbon__item_active');
+      categoryRef.classList.add('ribbon__item_active');
+      this.activeItem = categoryRef;
+      
+      let customEvent = new CustomEvent("ribbon-select", 
+        { detail: categoryRef.dataset.id, bubbles: true });
+      this.elem.dispatchEvent(customEvent);
+
+      return;
+    }
+  }
+
+  onScrollInner = () => {
+    
+    if (this.inner.scrollLeft) {
+      this.leftButton.classList.add('ribbon__arrow_visible');
+    } else {
+      this.leftButton.classList.remove('ribbon__arrow_visible');
+    }
+
+    let scrollRight = this.inner.scrollWidth - this.inner.scrollLeft - this.inner.clientWidth;    
+    if (scrollRight >= 1) {
+      this.rightButton.classList.add('ribbon__arrow_visible');
+    } else {
+      this.rightButton.classList.remove('ribbon__arrow_visible');
+    }    
   }
 }
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,58 @@
+import createElement from '../../assets/lib/create-element.js';
+
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    this.elem = createElement(
+      `<div class="slider">        
+        <div class="slider__thumb">
+          <span class="slider__value">${value}</span>
+        </div>       
+        <div class="slider__progress"></div>
+      </div>`);
+
+    this.value = value;
+    this.steps = steps - 1;
+
+    let sliderSteps = createElement(`<div class="slider__steps"></div>`);
+
+    for (let i=0; i<steps; i++) {
+      let span = createElement(`<span id="span${i}"></span>`);
+      if (i == value) {
+        span.classList.add('slider__step-active');
+        this.activeSpan = span;
+      }
+      sliderSteps.append(span);
+    }
+
+    this.elem.append(sliderSteps);
+
+    this.thumb = this.elem.querySelector('.slider__thumb');
+    this.progress = this.elem.querySelector('.slider__progress');
+    this.thumb.style.left = `0%`;
+    this.progress.style.width = `0%`;
+
+    this.elem.addEventListener('click', this.onClick);
+  }
+
+  onClick = (event) => {
+    let sliderData = this.elem.getBoundingClientRect();
+    this.value = Math.round(
+      (event.clientX - sliderData.left)/(sliderData.width/this.steps));
+    
+    let sliderValue = this.elem.querySelector('.slider__value');
+    sliderValue.textContent = this.value;
+
+    let activeSpan = document.getElementById(`span${this.value}`);
+    activeSpan.classList.add('slider__step-active');
+    this.activeSpan.classList.remove('slider__step-active');
+    this.activeSpan = activeSpan;   
+
+    let leftPercents = Math.round(100*this.value/this.steps);
+    this.thumb.style.left = `${leftPercents}%`;
+    this.progress.style.width = `${leftPercents}%`;
+
+    let customEvent = new CustomEvent("slider-change", 
+      { detail: this.value, bubbles: true });
+    this.elem.dispatchEvent(customEvent);
   }
 }
